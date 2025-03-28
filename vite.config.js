a568c4749710a0c824a0fdbb36ba9a5825232227
tuii-dev <tb@tuii.io>
2025-03-28 12:27:35 -0400

    Base path fix

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   test: {
-    environment: 'jsdom',
-    setupFiles: ['./vitest.setup.js'],
+    environment: "jsdom",
+    setupFiles: ["./vitest.setup.js"],
   },
-  base: '/'
-})
+  base: "/docker-gha/",
+});
